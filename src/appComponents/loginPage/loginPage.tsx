d6f9b42b1f625244e8e7be120bdee871c8db9430
Fabiano Tassotti <fabianotassotti@gmail.com>
2022-05-21 07:43:38 -0700

    updated name folder components to appComponents

---
+++ src/appComponents/loginPage/loginPage.tsx
@@ -0,0 +1,9 @@
+import './loginPage.css';
+
+export default function loginPage() {
+  return (
+      <>
+        <p>You can't get login yet.</p>
+      </>
+  );
+}
---
+++ src/appComponents/mainApp/mainApp.tsx
@@ -0,0 +1,13 @@
+//import React from 'react';
+//import logo from './logo.svg';
+import './mainApp.css';
+
+export default function mainApp() {
+  return (
+    <div className="mainApp">
+      <header className="App-header">
+          <a href='./loginPage'>Click here to go to the login page</a>
+      </header>
+    </div>
+  );
+}
