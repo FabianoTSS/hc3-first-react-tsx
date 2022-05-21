3a1dd164019ff995f625071832bad63998fce69b
Fabiano Tassotti <fabianotassotti@gmail.com>
2022-05-21 10:55:16 -0700

    Added nonExistentPage

---
+++ src/appComponents/nonExistentPage/nonExistentPage.tsx
@@ -0,0 +1,11 @@
+//import React from 'react';
+//import logo from './logo.svg';
+import './nonExistentPage.css';
+
+export default function nonExistentPage() {
+  return (
+    <>
+        <p>This page doesn't exist</p>
+    </>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import {  BrowserRouter,  Routes,  Route, } from "react-router-dom";
 import MainApp from "./appComponents/mainApp/mainApp";
 import LoginPage from "./appComponents/loginPage/loginPage";
+import NonExistentPage from "./appComponents/nonExistentPage/nonExistentPage";
 
 export default function App() {
   return (
@@ -10,7 +11,7 @@ export default function App() {
           <Route path="/" element={<MainApp />} />
           <Route index element={<MainApp />} />
           <Route path="loginPage" element={<LoginPage />} />
-          <Route path="*" element={<MainApp />} />
+          <Route path="*" element={<NonExistentPage />} />
       </Routes>
     </BrowserRouter>
   );
