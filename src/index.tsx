b386241c63885e6b0a93a4d9089f0c8f53191d0f
Fabiano Tassotti <fabianotassotti@gmail.com>
2022-05-21 13:32:25 -0700

    Styled components removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import {  BrowserRouter,  Routes,  Route, } from "react-router-dom";
+import {  BrowserRouter,  Routes,  Route } from "react-router-dom";
 import MainApp from "./appComponents/mainApp/mainApp";
 import LoginPage from "./appComponents/loginPage/loginPage";
 import NonExistentPage from "./appComponents/nonExistentPage/nonExistentPage";
@@ -16,6 +16,5 @@ export default function App() {
     </BrowserRouter>
   );
 }
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
